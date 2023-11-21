0e7d895a1a32126bdd3a0caefb2876598dae294d
chickencoc <solutw@gmail.com>
2023-11-21 22:14:39 +0900

    refactor #1 : 폴더 위치 변경
    - config -> global.config

--- src/main/java/com/example/money/global/config/WebSecurityConfig.java
+++ src/main/java/com/example/money/global/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.money.config;
+package com.example.money.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
