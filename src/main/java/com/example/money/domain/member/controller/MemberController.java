03f1f19dc6291e3b7d7e48615f48b41fe79b6c3c
chickencoc <solutw@gmail.com>
2023-12-05 14:20:20 +0900

    refactor #23 : singupDto 제한사항 추가

--- src/main/java/com/example/money/domain/member/controller/MemberController.java
+++ src/main/java/com/example/money/domain/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.example.money.domain.member.dto.MemberLoginRequestDto;
 import com.example.money.domain.member.dto.MemberSignupDto;
 import com.example.money.domain.member.dto.TokenResponseDto;
 import com.example.money.domain.member.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +18,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/signup")
-    public ResponseEntity<MemberSignupDto.Response> signup(@RequestBody MemberSignupDto.Request request) {
+    public ResponseEntity<MemberSignupDto.Response> signup(@Valid @RequestBody MemberSignupDto.Request request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(memberService.signup(request));
     }
 
